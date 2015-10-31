e68717d576f5f56027cebb3c912823feb2740040
Ankita Pise <aapise@ncsu.edu>
2015-10-31 19:15:48 -0400

    Updated Procedures

--- Project1/Procedures/display_student_profile.sql
+++ Project1/Procedures/display_student_profile.sql
@@ -1,5 +1,8 @@
-CREATE PROCEDURE `display_student_profile` ()
+CREATE DEFINER=`root`@`localhost` PROCEDURE `display_student_profile`(
+	IN user_id varchar(20)
+)
 BEGIN
 	SELECT *
-    FROM student_profile;
+    FROM student_profile
+    WHERE (student_profile.user_id = user_id);
 END
