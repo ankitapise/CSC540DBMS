cb70d675a398c9ef79866d960f15f543d85b8946
Abidaan <abidaan18@gmail.com>
2015-11-01 22:58:49 -0500

    Updated procedure for displaying all publications

--- Project1/Procedures/display_publications.sql
+++ Project1/Procedures/display_publications.sql
@@ -1,5 +1,6 @@
 CREATE PROCEDURE `display_publications` ()
 BEGIN
-	SELECT P.pub_id, P.title, P.category
+	SELECT P.pub_id, P.title, P.category,
+		   IF(P.e_copy = 1, 'Yes', 'No') AS e_copy
     FROM Publications P;
 END
