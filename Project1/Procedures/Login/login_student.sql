bda8386a3ac4d5badc2ba2858c720d1637c22ebd
Ankita Pise <aapise@ncsu.edu>
2015-11-02 22:40:34 -0500

    login procedure changed

--- Project1/Procedures/Login/login_student.sql
+++ Project1/Procedures/Login/login_student.sql
@@ -3,7 +3,7 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `login_student`(
     IN password varchar(20)
 )
 BEGIN
-	SELECT P.user_id, P.password
+	SELECT P.patron_id, P.user_id, P.password
 	FROM Patrons P, Students S
 	WHERE (P.user_id = user_id) AND (P.password = password) AND (P.patron_id = S.student_id);
 END
